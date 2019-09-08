838ff49872a0b26c8d7b5b7f5c4e1ed696effec2
CremerFreddy <frederic.cremer@web.de>
2019-09-08 20:30:13 +0200

    select schachfelder

---
+++ Assets/controlsManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlsManager : MonoBehaviour
+{
+    public bool cardSelected;
+    public schachbrett brett;
+    public Camera cam;
+    public schachfeld selectedfield;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardSelected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cardSelected)
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag == "chessfield")
+                {
+                    if (hit.collider.GetComponent<schachfeld>().Equals(selectedfield))
+                    {
+
+                    }
+                    else
+                    {
+                        if(selectedfield != null)
+                        {
+                            selectedfield.selected = false;
+                        }
+                        
+                        selectedfield = hit.collider.GetComponent<schachfeld>();
+                        selectedfield.selected = true;
+                    }
+                    
+                }
+                
+
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+        
+    }
+}
--- Assets/schachfeld.cs
+++ Assets/schachfeld.cs
@@ -9,17 +9,31 @@ public class schachfeld : MonoBehaviour
     public bool locked;
     public AI onField;
     public schachbrett brett;
+    public bool selected;
+    public bool changed;
+    public 
     // Start is called before the first frame update
     void Start()
     {
         locked = false;
         onField = null;
+        selected = false;
+        changed = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(selected && changed)
+        {
+            changed = false;
+            transform.position = new Vector3(transform.position.x, 0.2f, transform.position.z);
+        }
+        else if(!selected && !changed)
+        {
+            changed = true;
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
     }
 
     public void lockField(bool ok)
